3c4056bc302ead212dcae7e47c0718e0cba37130
Alexander Lee <alexanderwlee@gmail.com>
2021-04-06 01:09:02 -0400

    add typings

--- src/fhm.py
+++ src/fhm.py
@@ -1,60 +1,158 @@
 import sys
 import time
-from fhm_utils import get_item_twu_dict, get_I_star, get_util_lists_and_EUCS, get_e_in_P
+from typing import Union, List
+from fhm_utils import UtilList, UtilListElem, ItemUtilPair
 
 
-def fhm(db_path, minutil):
-    item_twu_dict = get_item_twu_dict(db_path)
-    I_star = get_I_star(item_twu_dict, minutil)
-    util_lists, EUCS = get_util_lists_and_EUCS(db_path, item_twu_dict, I_star)
-    hu_itemsets = []
-    search((), I_star.keys(), minutil, I_star, util_lists, EUCS, hu_itemsets)
-    return hu_itemsets
+class FHM:
+    def __init__(self, db_path: str, minutil: int) -> None:
+        self.db_path = db_path
+        self.minutil = minutil
+        self.EUCS = {}
+        self.itemset_buffer = [0] * 200
+        self.hui_count = 0
+        self.runtime = 0
 
+    def run(self) -> None:
+        time_start = time.time()
+        # first DB scan to get TWU of each item
+        item_twu_dict = {}
+        with open(self.db_path) as db:
+            for transac in db:
+                transac_data = transac.split(":")
+                items = transac_data[0].split()
+                transac_util = int(transac_data[1])
+                for item in items:
+                    item = int(item)
+                    if item in item_twu_dict:
+                        item_twu_dict[item] += transac_util
+                    else:
+                        item_twu_dict[item] = transac_util
 
-def search(P, extens_of_P, minutil, I_star, util_lists, EUCS, hu_itemsets):
-    for Px in extens_of_P:
-        Px_iutil_sum = sum([ex[1] for ex in util_lists[Px]])
-        if Px_iutil_sum >= minutil:
-            hu_itemsets.append((Px, Px_iutil_sum))
-        Px_rutil_sum = sum([ex[2] for ex in util_lists[Px]])
-        if Px_iutil_sum + Px_rutil_sum >= minutil:
-            extens_of_Px = list()
-            for Py in extens_of_P:
-                x = Px[-1]
-                y = Py[-1]
-                if I_star[(y,)] > I_star[(x,)]:
+        # create list of utility lists and
+        # map of itemsets to utility lists
+        util_lists = []
+        itemset_util_list_dict = {}
+        for item, twu in item_twu_dict.items():
+            if twu >= self.minutil:
+                util_list = UtilList(item)
+                itemset_util_list_dict[item] = util_list
+                util_lists.append(util_list)
+
+        # sort util_lists in order of ascending TWU
+        util_lists.sort(key=lambda ul: item_twu_dict[ul.item])
+
+        # second DB scan to populate utility lists and EUCS
+        with open(self.db_path) as db:
+            for tid, transac in enumerate(db):
+                transac_data = transac.split(":")
+                items = transac_data[0].split()
+                transac_util = int(transac_data[1])
+                item_utils = transac_data[2].split()
+
+                # get items that have TWU >= minutil
+                item_pairs = []
+                rutil = 0
+                for i in range(len(items)):
+                    item_util_pair = ItemUtilPair(int(items[i]), int(item_utils[i]))
+                    if item_twu_dict[item_util_pair.item] >= self.minutil:
+                        item_pairs.append(item_util_pair)
+                        rutil += item_util_pair.util
+                item_pairs.sort(key=lambda p: item_twu_dict[p.item])
+
+                # add items to utility lists
+                for i in range(len(item_pairs)):
+                    rutil -= item_pairs[i].util
+                    util_list_elem = UtilListElem(tid, item_pairs[i].util, rutil)
+                    itemset_util_list_dict[item_pairs[i].item].add_elem(util_list_elem)
+
+                    # create EUCS
+                    for j in range(i + 1, len(item_pairs)):
+                        item = item_pairs[i].item
+                        next_item = item_pairs[j].item
+                        if item in self.EUCS:
+                            if next_item in self.EUCS[item]:
+                                self.EUCS[item][next_item] += transac_util
+                            else:
+                                self.EUCS[item][next_item] = transac_util
+                        else:
+                            self.EUCS[item] = {next_item: transac_util}
+        self.search(self.itemset_buffer, 0, None, util_lists)
+        self.runtime = (time.time() - time_start) * 1000
+
+    def search(
+        self,
+        prefix: List[int],
+        prefix_len: int,
+        prefix_util_list: Union[UtilList, None],
+        util_lists: List[UtilList],
+    ) -> None:
+        for i in range(len(util_lists)):
+            X = util_lists[i]
+            if X.sum_iutils >= self.minutil:
+                self.output(prefix, prefix_len, X.item, X.sum_iutils)
+            if X.sum_iutils + X.sum_rutils >= self.minutil:
+                ex_util_lists = []
+                for j in range(len(util_lists)):
+                    Y = util_lists[j]
                     try:
-                        if EUCS[x][y] >= minutil:
-                            Pxy = tuple(list(P) + [x, y])
-                            util_lists[Pxy] = construct(P, Px, Py, util_lists)
-                            extens_of_Px.append(Pxy)
+                        if self.EUCS[X.item][Y.item] >= self.minutil:
+                            Pxy_util_list = self.construct(prefix_util_list, X, Y)
+                            ex_util_lists.append(Pxy_util_list)
                     except KeyError:
                         continue
-            search(Px, extens_of_Px, minutil, I_star, util_lists, EUCS, hu_itemsets)
+                try:
+                    self.itemset_buffer[prefix_len] = X.item
+                except IndexError:
+                    sys.exit("Error: length itemset_buffer is too small")
+                self.search(self.itemset_buffer, prefix_len + 1, X, ex_util_lists)
 
+    def output(self, prefix: List[int], prefix_len: int, item: int, util: int) -> None:
+        self.hui_count += 1
+        out = []
+        for i in range(prefix_len):
+            out.append(str(prefix[i]))
+        out.append(str(item))
+        out.append("#UTIL:")
+        out.append(str(util))
+        print(" ".join(out))
 
-def construct(P, Px, Py, util_lists):
-    util_list_of_Pxy = []
-    for ex in util_lists[Px]:
-        for ey in util_lists[Py]:
-            if ex[0] == ey[0]:
-                if P in util_lists:
-                    e = get_e_in_P(P, ex, util_lists)
-                    exy = (ex[0], ex[1] + ey[1] - e[1], ey[2])
-                else:
-                    exy = (ex[0], ex[1] + ey[1], ey[2])
-                util_list_of_Pxy.append(exy)
-    return util_list_of_Pxy
+    def construct(self, P: Union[UtilList, None], Px: UtilList, Py: UtilList) -> UtilList:
+        Pxy_util_list = UtilList(Py.item)
+        for ex in Px.elems:
+            ey = self.get_elem_with_tid(Py, ex.tid)
+            if ey is None:
+                continue
+            if P is None:
+                exy = UtilListElem(ex.tid, ex.iutil + ey.iutil, ey.rutil)
+                Pxy_util_list.add_elem(exy)
+            else:
+                e = self.get_elem_with_tid(P, ex.tid)
+                if e is not None:
+                    exy = UtilListElem(ex.tid, ex.iutil + ey.iutil - e.iutil, ey.rutil)
+                    Pxy_util_list.add_elem(exy)
+        return Pxy_util_list
+
+    def get_elem_with_tid(self, util_list: UtilList, tid: int) -> Union[UtilListElem, None]:
+        elems = util_list.elems
+        first = 0
+        last = len(elems) - 1
+        while first <= last:
+            mid = (first + last) // 2
+            if elems[mid].tid < tid:
+                first = mid + 1
+            elif elems[mid].tid > tid:
+                last = mid - 1
+            else:
+                return elems[mid]
+
+    def get_stats(self) -> None:
+        print(f"hui_count: {self.hui_count}")
+        print(f"runtime (ms): {self.runtime}")
 
 
 if __name__ == "__main__":
     args = sys.argv
-    start_time = time.time()
-    hu_itemsets = fhm(db_path=args[1], minutil=int(args[2]))
-    end_time = time.time()
-    hu_itemsets.sort(key=lambda t: t[0][0], reverse=True)
-    print(f"hu_itemsets: {hu_itemsets}")
-    print(f"count: {len(hu_itemsets)}")
-    total_time = (end_time - start_time) * 1000
-    print(f"time (ms): {total_time}")
+    fhm = FHM(db_path=args[1], minutil=int(args[2]))
+    fhm.run()
+    fhm.get_stats()
--- src/fhm_utils.py
+++ src/fhm_utils.py
@@ -1,67 +1,24 @@
-def get_item_twu_dict(db_path):
-    item_twu_dict = {}
-    with open(db_path) as db:
-        for transac in db:
-            transac_data = transac.split(":")
-            items = transac_data[0].split()
-            transac_util = int(transac_data[1])
-            for item in items:
-                item = int(item)
-                if item in item_twu_dict:
-                    item_twu_dict[item] += transac_util
-                else:
-                    item_twu_dict[item] = transac_util
-    return item_twu_dict
+class UtilListElem:
+    def __init__(self, tid: int, iutil: int, rutil: int) -> None:
+        self.tid = tid
+        self.iutil = iutil
+        self.rutil = rutil
 
 
-def get_I_star(item_twu_dict, minutil):
-    I_star = {}
-    for item, twu in item_twu_dict.items():
-        if twu >= minutil:
-            I_star[(item,)] = twu
-    return I_star
+class UtilList:
+    def __init__(self, item: int) -> None:
+        self.item = item
+        self.sum_iutils = 0
+        self.sum_rutils = 0
+        self.elems = []
+
+    def add_elem(self, elem: UtilListElem) -> None:
+        self.elems.append(elem)
+        self.sum_iutils += elem.iutil
+        self.sum_rutils += elem.rutil
 
 
-def get_util_lists_and_EUCS(db_path, item_twu_dict, I_star):
-    util_lists = {}
-    EUCS = {}
-    with open(db_path) as db:
-        for tid, transac in enumerate(db):
-            transac_data = transac.split(":")
-            items = transac_data[0].split()
-            transac_util = int(transac_data[1])
-            item_utils = transac_data[2].split()
-            item_util_tuples = list(zip(items, item_utils))
-            item_util_tuples.sort(key=lambda t: item_twu_dict[int(t[0])])
-            for i, item_util_tuple in enumerate(item_util_tuples):
-                item = int(item_util_tuple[0])
-                itemset = (item,)
-                if itemset in I_star:
-                    iutil = int(item_util_tuple[1])
-                    rutil = 0
-                    for j in range(i + 1, len(item_util_tuples)):
-                        rutil += int(item_util_tuples[j][1])
-                        next_item = int(item_util_tuples[j][0])
-                        add_to_EUCS(item, next_item, EUCS, transac_util)
-                    util_list_tuple = (tid, iutil, rutil)
-                    if itemset in util_lists:
-                        util_lists[itemset].append(util_list_tuple)
-                    else:
-                        util_lists[itemset] = [util_list_tuple]
-    return util_lists, EUCS
-
-
-def add_to_EUCS(item, next_item, EUCS, transac_util):
-    if item in EUCS:
-        if next_item in EUCS[item]:
-            EUCS[item][next_item] += transac_util
-        else:
-            EUCS[item][next_item] = transac_util
-    else:
-        EUCS[item] = {next_item: transac_util}
-
-
-def get_e_in_P(P, ex, util_lists):
-    for e in util_lists[P]:
-        if e[0] == ex[0]:
-            return e
+class ItemUtilPair:
+    def __init__(self, item: int, util: int) -> None:
+        self.item = item
+        self.util = util
