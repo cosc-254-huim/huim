57fd9c77d92229cb9004eba6368fda8f5df94827
michaelbakshandeh <mikebday99@gmail.com>
2021-05-17 22:05:11 -0400

    changing data structure

--- src/two_phase.py
+++ src/two_phase.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import time
+import csv
 from memory_profiler import memory_usage
 
 
@@ -11,12 +12,13 @@ class TwoPhase:
         self.minutil = minutil
         self.itemset_buffer_size = 200
         self.hui_count = 0
+        self.prune_count = 0
         self.candidate_count = 0
         self.start_time = 0
         self.end_time = 0
 
     def run(self) -> None:
-        self.mem_usage = memory_usage(self.twophase)
+        self.mem_usage = memory_usage(self.twophase, max_usage=True, max_iterations=1)
 
     def twophase(self) -> None:
         self.start_time = time.time()
@@ -24,6 +26,7 @@ class TwoPhase:
         item_twu_dict = {}
 
         # phase one
+        # scan the db to get the twu of all items
         with open(self.input_path) as db:
             for transac in db:
                 transac_data = transac.split(":")
@@ -36,62 +39,76 @@ class TwoPhase:
                     else:
                         item_twu_dict[item] = transac_util
 
+        print("generating candidates now")
+        print(self.minutil)
+        # generate candidate set of size 1
         candidates = set()
         for item in item_twu_dict:
             if item_twu_dict.get(item) > self.minutil:
-                to_add = set()
-                to_add.add(item)
-                candidates.add(to_add)
-                all_candidates.add(to_add)
+                candidates.add(frozenset({item}))
+                all_candidates.add(frozenset({item}))
 
         item_twu_dict.clear()
-        generated = set()
-        pruned = set()
         k = 2
 
         # apriori stuff
         while len(candidates) > 0:
-            generated.clear()
-            pruned.clear()
+            generated = set()
+            pruned = set()
             # generate
+            print("generate")
             for item_set in candidates:
+                temp1 = set(item_set)
                 for item_set_2 in candidates:
-                    new_set = set.union(item_set_2, item_set)
+                    # convert back to set
+                    temp2 = set(item_set_2)
+                    # take the union
+                    new_set = set.union(temp1, temp2)
+                    # if set is right length, add to generated
                     if len(new_set) == k:
-                        generated.add(new_set)
+                        generated.add(frozenset(new_set))
             # prune
+            print("prune")
             for item_set in generated:
                 add = True
                 for item in item_set:
-                    check = set()
-                    check.union(generated)
+                    # creates temporary set of size k-1
+                    check = set(item_set)
                     check.remove(item)
+
+                    # checks to see if its in the set
                     if check not in candidates:
                         add = False
                 if add:
-                    pruned.add(item_set)
+                    pruned.add(frozenset(item_set))
+            
+            print("done with prune")
+            # add pruned itemset to global prune counter
+            self.prune_count += len(generated) - len(pruned)
+
+            print("clear")
+            print(len(pruned))
             # clear candidates
             candidates.clear()
             # get utility
+            print("getting utilities")
             with open(self.input_path) as db:
                 for transac in db:
-                    transaction = set()
                     transac_data = transac.split(":")
-                    items = transac_data[0].split()
+                    transaction = set(transac_data[0].split())
                     transac_util = int(transac_data[1])
-                    for item in items:
-                        item = int(item)
-                        transaction.add(item)
                     for item_set in pruned:
                         if all(elem in transaction for elem in item_set):
-                            item_twu_dict[item_set] += transac_util
-                        else:
-                            item_twu_dict[item_set] = transac_util
+                            if item_set in item_twu_dict:
+                                item_twu_dict[item_set] += transac_util
+                            else:
+                                item_twu_dict[item_set] = transac_util
+            print("adding to candidates")
             # add to candidates
             for item_set in item_twu_dict:
                 if item_twu_dict.get(item_set) > self.minutil:
-                    candidates.add(item_set)
-                    all_candidates.add(item_set)
+                    candidates.add(frozenset({item_set}))
+                    all_candidates.add(frozenset({item_set}))
             item_twu_dict.clear()
             k += 1
 
@@ -121,15 +138,17 @@ class TwoPhase:
 
     def print_stats(self) -> None:
         print("===============Two Phase ALGORITHM STATS===============")
+        self.runtime = (self.end_time - self.start_time) * 1000
         print(f"total runtime (ms): {(self.end_time - self.start_time) * 1_000}")
         print(f"high utility itemset count: {self.hui_count}")
         print(f"candidate itemset count: {self.candidate_count}")
+        print(f"pruned itemset count: {self.prune_count}")
         print(f"maximum memory used (MB): {max(self.mem_usage)}")
 
     # initializes csv file by adding column names if csv file does not exist
     def initialize_csv(self, filename) -> None:
         fields = ['minimum utility', 'total runtime (ms)', 'total itemSet count', 'high utility itemSet count',
-                  'candidate itemSet count', 'pruned itemSet count', 'maximum memory used (MB)']
+                  'pruned itemSet count', 'maximum memory used (MB)']
 
         with open(filename, "w+") as csvfile:
             # creating a csv writer object
@@ -142,7 +161,6 @@ class TwoPhase:
     def experiment(self, filename) -> None:
         # data rows of csv file
         rows = [[self.minutil, self.runtime,
-                self.itemset_count,
                 self.hui_count,
                 self.candidate_count,
                 self.prune_count,
@@ -160,52 +178,53 @@ if __name__ == "__main__":
     twoPhase = TwoPhase(input_path=args[1], output_path=args[2], minutil=int(args[3]))
     twoPhase.run()
     twoPhase.print_stats()
+    input_path = args[1]
 
     # initializes csv file by adding column names if csv file does not exist
     # adds rows to the csv file
     # names the csv file after the name of the input file
     # fetch the current directory
     cur_path = os.getcwd()
-    cur_path1 = cur_path.split("/")[:-1] # ensures that we are no longer in src and are instead in 
+    cur_path1 = cur_path.split("/")[:-1] # ensures that we are no longer in src and are instead in experiments
 
     if not os.path.isdir("/".join(cur_path1)+"/experiments"):
         os.mkdir("/".join(cur_path1)+"/experiments")
     os.chdir("/".join(cur_path1)+"/experiments")
 
     if "chainstore.txt" in input_path:
-        if not os.path.exists("experiment_chain_store.csv"):
-            fhm.initialize_csv("experiment_chain_store.csv")
-        fhm.experiment("experiment_chain_store.csv")
+        if not os.path.exists("experiment_chain_store2.csv"):
+            twoPhase.initialize_csv("experiment_chain_store2.csv")
+        twoPhase.experiment("experiment_chain_store2.csv")
 
     if "DB_Utility.txt" in input_path:
-        if not os.path.exists("experiment_DB_Utility.csv"):
-            fhm.initialize_csv("experiment_DB_Utility.csv")
-        fhm.experiment("experiment_DB_Utility.csv")
+        if not os.path.exists("experiment_DB_Utility2.csv"):
+            twoPhase.initialize_csv("experiment_DB_Utility2.csv")
+        twoPhase.experiment("experiment_DB_Utility2.csv")
 
     if "ecommerce_utility_no_timestamps.txt" in input_path:
-        if not os.path.exists("experiment_ecommerce_utility_no_timestamps.csv"):
-            fhm.initialize_csv("experiment_ecommerce_utility_no_timestamps.csv")
-        fhm.experiment("experiment_ecommerce_utility_no_timestamps.csv")
+        if not os.path.exists("experiment_ecommerce_utility_no_timestamps2.csv"):
+            twoPhase.initialize_csv("experiment_ecommerce_utility_no_timestamps2.csv")
+        twoPhase.experiment("experiment_ecommerce_utility_no_timestamps2.csv")
 
     if "foodmart.txt" in input_path:
-        if not os.path.exists("experiment_food_mart.csv"):
-            fhm.initialize_csv("experiment_food_mart.csv")
-        fhm.experiment("experiment_food_mart.csv")
+        if not os.path.exists("experiment_food_mart2.csv"):
+            twoPhase.initialize_csv("experiment_food_mart2.csv")
+        twoPhase.experiment("experiment_food_mart2.csv")
 
     if "retail.txt" in input_path:
-        if not os.path.exists("experiment_retail.csv"):
-            fhm.initialize_csv("experiment_retail.csv")
-        fhm.experiment("experiment_retail.csv")
+        if not os.path.exists("experiment_retail2.csv"):
+            twoPhase.initialize_csv("experiment_retail2.csv")
+        twoPhase.experiment("experiment_retail2.csv")
     
     if "kosarak.txt" in input_path:
-        if not os.path.exists("experiment_kosarak.csv"):
-            fhm.initialize_csv("experiment_kosarak.csv")
-        fhm.experiment("experiment_kosarak.csv")
+        if not os.path.exists("experiment_kosarak2.csv"):
+            twoPhase.initialize_csv("experiment_kosarak2.csv")
+        twoPhase.experiment("experiment_kosarak2.csv")
 
     if "chess.txt" in input_path:
-        if not os.path.exists("experiment_chess.csv"):
-            fhm.initialize_csv("experiment_chess.csv")
-        fhm.experiment("experiment_chess.csv")
+        if not os.path.exists("experiment_chess2.csv"):
+            twoPhase.initialize_csv("experiment_chess2.csv")
+        twoPhase.experiment("experiment_chess2.csv")
 
     # return to the original directory so the shell script can correctly find the path
     cur_path = os.getcwd().split("/")[:-1]
